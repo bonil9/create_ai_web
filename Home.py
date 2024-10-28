0e4ed68d2a389a6a3734f2364e900278b8726fa1
SYScienceAI <155238313+SYScienceAI@users.noreply.github.com>
2024-10-28 03:45:21 +0000

    공공131111112

--- Home.py
+++ Home.py
@@ -3,4 +3,4 @@ import streamlit as st
 
 
 # 첫 화면 문구 추가
-st.write("RecordAI: 저는 물리를 좋아하는 과학교사 입니다. 특히 양자역학을, 많은 것을 기록하고자 합니다.")
+st.write("RecordAI: 저는 물리를 좋아하는 과학교사 입니다. 특히 양자역학을, 많은 것을 기록하고자 합니다. 요즘 에듀테크 중 MBL 가능한 것과 ChatGPT에 중점을 둡니다.")
