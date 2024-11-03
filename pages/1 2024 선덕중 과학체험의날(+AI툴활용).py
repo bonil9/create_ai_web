2ca13de23a0806c0a97932becd484b67c1e16c92
SYScienceAI <155238313+SYScienceAI@users.noreply.github.com>
2024-11-03 03:20:26 +0000

    타이틀 크기

--- pages/1 2024 선덕중 과학체험의날(+AI툴활용).py
+++ pages/1 2024 선덕중 과학체험의날(+AI툴활용).py
@@ -1,7 +1,23 @@
 import streamlit as st
 
-# Streamlit 페이지 제목 설정
-st.title("2024 선덕중학교 과학 체험의 날 행사(AI툴을 이용,시나리오가 있는)")
+# 페이지 구성 설정을 위한 스타일링 적용
+st.markdown(
+    """
+    <style>
+    .title {
+        font-size: 2em;
+        text-align: center;
+        width: 100%;
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# 사용자 정의 스타일이 적용된 타이틀 텍스트
+st.markdown('<div class="title">2024 선덕중학교 과학 체험의 날 행사(AI툴 사용)</div>', unsafe_allow_html=True)
 
 # 링크 추가
 chatgpt_url_1 = "https://blog.naver.com/sayment/223413856862"
