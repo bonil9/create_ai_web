bcd0a703fd57ac7d09e6212905abd03d2fbb648e
SYScienceAI <155238313+SYScienceAI@users.noreply.github.com>
2025-01-17 03:56:39 +0000

    오타 수정

--- pages/3 지능형과학실ON.py
+++ pages/3 지능형과학실ON.py
@@ -9,7 +9,7 @@ chatgpt_url_2 = "https://blog.naver.com/sayment/223675587691"
 chatgpt_url_3 = "https://blog.naver.com/sayment/223726624617"
 
 
-st.markdown(f'[학교 관리자 권한 받]({chatgpt_url_1})', unsafe_allow_html=True)
+st.markdown(f'[학교 관리자 권한 받기!!]({chatgpt_url_1})', unsafe_allow_html=True)
 st.markdown(f'[지능형과학실ON 사용중 당황하게 만드는 것들]({chatgpt_url_2})', unsafe_allow_html=True)
 st.markdown(f'[협력탐구 설정하는 해야하는 경우]({chatgpt_url_3})', unsafe_allow_html=True)
 
