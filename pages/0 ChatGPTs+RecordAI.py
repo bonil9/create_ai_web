2f1e5eadb713a82ebe1374ab03d6df4d47aed919
SYScienceAI <155238313+SYScienceAI@users.noreply.github.com>
2025-02-04 01:04:15 +0000

    gpts 추가

--- pages/0 ChatGPTs+RecordAI.py
+++ pages/0 ChatGPTs+RecordAI.py
@@ -7,7 +7,12 @@ st.title("ChatGPTs + RecordAI")
 chatgpt_url_1 = "https://chatgpt.com/g/g-PeGH8Y8Mi-hagsaengbu-saenggibu-jagseonghaedeuryeoyo-haengbal-seteug-dongari-seupoceukeulreob-jayuhaggi-jayul-codeung-junghaggyo-godeunghaggyo"
 chatgpt_url_2 = "https://chatgpt.com/g/g-MFJnyFAFQ-siheommunje-danweonpyeongga-siheomculje-gosaculje-gyosa"
 chatgpt_url_3 = "https://chatgpt.com/g/g-9N4SjezVJ-self-introduction-writing-recruitment"
+chatgpt_url_4 = "https://chatgpt.com/g/g-nh5M17KeN-has-syoping-hiteu-sangpum-mogrog-cuceon-silsigan-neibeo-geomsaeg-giban"
+chatgpt_url_5 = "https://chatgpt.com/g/g-67a0b8c7892881919c43fdb9385e8107-geurim-jaesaengsan-imiteisyeon"
+
 
 st.markdown(f'[생기부 작성 ChatGPTs 보러 가기]({chatgpt_url_1})', unsafe_allow_html=True)
 st.markdown(f'[시험 출제 ChatGPTs 보러 가기]({chatgpt_url_2})', unsafe_allow_html=True)
 st.markdown(f'[영어 자기소개서 ChatGPTs 보러 가기]({chatgpt_url_3})', unsafe_allow_html=True)
+st.markdown(f'[네이버 검색 기반 상품 추천 ChatGPTs 보러 가기]({chatgpt_url_4})', unsafe_allow_html=True)
+st.markdown(f'[사진(그림) 변형 이미테이션 ChatGPTs 보러 가기]({chatgpt_url_5})', unsafe_allow_html=True)
