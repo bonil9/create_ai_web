c6350668c2f6551e19c4845bafc91e6b5d4c8a68
SYScienceAI <155238313+SYScienceAI@users.noreply.github.com>
2025-02-09 05:13:53 +0000

    1212

--- pages/8 에라토스테네스 지구크기측정 -각도 알려줌.py
+++ pages/8 에라토스테네스 지구크기측정 -각도 알려줌.py
@@ -10,7 +10,8 @@ def calculate_angle(base, height):
     
     return theta_deg
 
-st.title("에라토스테네스의 지구크기 측정--각도 계산")
+st.title("에라토스테네스의 지구크기 측정 각도 계산")
+
 
 base = st.number_input("막대기 길이를 입력하세요:(예시:12.2)(단위는 쓰지마세요)", min_value=0.0, format="%.2f")
 height = st.number_input("그림자 길이를 입력하세요:(예시:3.2)(단위는 쓰지마세요)", min_value=0.0, format="%.2f")
